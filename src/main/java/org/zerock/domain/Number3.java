24eac2026f832938c2803b84d90e889ab558aed7
mit <mit@DESKTOP-5ULJNMC>
2020-04-06 11:16:20 +0900

    최초 업로드

---
+++ src/main/java/org/zerock/domain/Number3.java
@@ -0,0 +1,11 @@
+package org.zerock.domain;
+
+import lombok.Data;
+
+@Data
+public class Number3 {
+	private int num1;
+	private int num2;
+	private int num3;
+	
+}
---
+++ src/test/java/org/zerock/service/BoardServiceTests.java
@@ -0,0 +1,42 @@
+package org.zerock.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.Criteria;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class) 
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml") 
+@Log4j
+public class BoardServiceTests {
+
+	@Setter(onMethod_= {@Autowired})
+	private BoardService service; 
+	
+	@Test
+	public void testExist() {
+		log.info(service);
+		assertNotNull(service);
+	}
+	
+	
+	@Test
+	public void testGetList() {
+		service.getList(new Criteria(1,10)).forEach(board->log.info(board));
+	}
+	
+	@Test
+	public void testGet() {
+		service.get(3);
+		
+	}
+	
+}
+
